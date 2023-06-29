a73c38dbfc5b8d722291c34c0179c51bcfb5530d
Akshit Gulyan <akshitgulyan@gmail.com>
2023-06-29 18:14:40 +0530

    add post

--- blogger/blog/urls.py
+++ blogger/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # from . import views
-from .views import HomeView, postdetailsview
+from .views import HomeView, postdetailsview,addpostview,form_submit
 
 urlpatterns = [
     # path('', views.home, name="home"),
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', postdetailsview.as_view(), name='postdetails'),
+    path('addpost/', addpostview.as_view(), name='addpost'),
+    path('submit/', form_submit, name='form_submit'),
 ]
--- blogger/blog/views.py
+++ blogger/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render,redirect
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from . import templates
+from django.contrib.auth.models import User
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -12,3 +14,25 @@ class HomeView(ListView):
 class postdetailsview(DetailView):
     model=Post
     template_name = 'postdetails.html'
+
+class addpostview(CreateView):
+    model=Post
+    template_name = 'addpost.html'
+    fields = '__all__'
+
+def form_submit(request):
+    if request.method == 'POST':
+        title_val = request.POST.get('title')
+        user_val = request.POST.get('username')
+        email_val = request.POST.get('email')
+        password_val = request.POST.get('password')
+        firstname_val = request.POST.get('first_name')
+        lastname_val = request.POST.get('last_name')
+        body_val = request.POST.get('body')
+        user = User.objects.create_user(username=user_val, email=email_val, password=password_val, first_name=firstname_val, last_name=lastname_val)
+        Post.objects.create(title=title_val, author=user, body=body_val)
+        
+        # Redirect to a success page or another URL
+        return redirect('home')
+
+    return render(request, 'templates/addpost.html')
